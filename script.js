174a2ed4e53e7d9efe3308e840ea7ebb07adeb20
KumanEnrique <36672948+KumanEnrique@users.noreply.github.com>
2020-04-24 18:29:08 -0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,16 @@
+replaceText(document.body)
+
+function replaceText(element){
+    if(element.hasChildNodes()){
+        element.childNodes.forEach(replaceText)
+    }else if(element.nodeType === Text.TEXT_NODE){
+        if(element.textContent.match(/coronavirus/gi)){
+            const newElement = document.createElement("span")
+            newElement.innerHTML = element.textContent.replace(/(coronavirus)/gi,`<span class="rainbow">$1</span>`)
+            element.replaceWith(newElement)
+           /*  element.parentElement.style.color = "red"
+            element.parentElement.style.backgroundColor = "red" */
+        }
+        // element.textContent = element.textContent.replace(/coronavirus/gi,"xxxx")
+    }
+}
